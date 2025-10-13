18f3cc314dc87a45710f764da5bf5144904a5796
a13k5-tilsig <aleksander.knoph@tilsig.com>
2025-10-13 19:03:00 +0200

    Cleanup.

--- src/lib/tilgridlib/Tilgrid.svelte
+++ src/lib/tilgridlib/Tilgrid.svelte
@@ -1,29 +1,16 @@
-<script module lang="ts">
-	import { type IWidget } from './Widget.svelte';
-
-	export interface IFuncs {
-		add?: () => void;
-		remove?: (id: Pick<IWidget, 'id'>) => void;
-		move?: (id: Pick<IWidget, 'id'>) => void;
-		size?: (id: Pick<IWidget, 'id'>) => void;
-	}
-</script>
-
 <script lang="ts">
+	import type { IWidget, IFuncs } from './Widget.svelte';
 	import Widget from './Widget.svelte';
 
 	const DEFAULT_WIDTH: string = '100%';
 	const DEFAULT_HEIGHT: string = '100%';
-	const DEFAULT_WIDGETS: IWidget[] = [];
-
-	// The surrounding area steps where the ghost will snapp.
 	const DEFAULT_SNAPPING_AREA: number = 50;
 
 	interface Props {
 		w?: string;
 		h?: string;
 		snappingArea?: number;
-		widgets?: IWidget[];
+		widgets: IWidget[];
 		funcs?: IFuncs;
 	}
 
@@ -31,18 +18,17 @@
 		w = DEFAULT_WIDTH,
 		h = DEFAULT_HEIGHT,
 		snappingArea = DEFAULT_SNAPPING_AREA,
-		widgets = $bindable(DEFAULT_WIDGETS),
+		widgets = $bindable(),
 		funcs
 	}: Props = $props();
 
-	// Compansate the snapping area in order to align the "grid".
+	// Fix the alignment of the snapping-points to start from 0.0.
 	let snappingAlignComp: string = $derived((snappingArea / 2).toFixed());
 
 	let moving: boolean = $state(true);
 </script>
 
 <div
-	class="wrapper"
 	class:snapp-hints={moving}
 	style:width={w}
 	style:height={h}
@@ -51,14 +37,13 @@
 		--snapping-align-comp: {snappingAlignComp}px;
 	"
 >
-	{#each widgets as spec, i}
-		<Widget bind:spec={widgets[i]} {snappingArea} />
+	{#each widgets as _, i}
+		<Widget bind:spec={widgets[i]} {snappingArea} {funcs} />
 	{/each}
 </div>
 
 <style>
 	.snapp-hints {
-		background-color: lightgray;
 		background-image: radial-gradient(black 1px, transparent 0);
 		background-size: var(--snapping-area) var(--snapping-area);
 		background-position: var(--snapping-align-comp) var(--snapping-align-comp);

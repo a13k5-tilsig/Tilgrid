73c6b3ad8dbf636b65c0ff3759ec22fc1065eb6e
a13k5-tilsig <aleksander.knoph@tilsig.com>
2025-10-14 13:48:47 +0200

    Some documentation.

--- src/lib/tilgridlib/types/widget.ts
+++ src/lib/tilgridlib/types/widget.ts
@@ -12,6 +12,12 @@ export interface IWidget extends IPosition, ISize {
 	id: number;
 }
 
+/**
+ * add?: Runs after a widget is added to the container.
+ * remove?: Runs after a widget is removed from the container.
+ * move?: Runs after a widget is moved inside the container.
+ * size?: Runs after a widget is resized inside the container.
+ */
 export interface IFuncs {
 	add?: () => void;
 	remove?: (id: Pick<IWidget, 'id'>) => void;
